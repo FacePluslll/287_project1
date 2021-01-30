edf1b7e658e8f5002657b63eb535b0cad4aa5f8c
nm811 <nmetlapalli1@gmail.com>
2021-01-30 00:56:01 -0600

    Completed the edit name method -- this method is used to edit the name of a movie

---
+++ Movie.java
@@ -0,0 +1,51 @@
+
+package project_1;
+
+import java.util.*;
+import java.text.*;
+
+
+public class Movie implements Comparable<Movie> {
+	private Date releaseDate;
+	private String name;
+	private String description;
+	private Date receiveDate;
+	enum Status {
+		RECEIVED,
+		RELEASED
+	}
+	private Status status;
+	// mov = new Movie(name, releaseDate, description, receiveDate, strStatus);
+	public Movie(String name, Date releaseDate, String description, Date receiveDate, String strStatus) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.description = description;
+		this.receiveDate = receiveDate;
+		this.status = Status.valueOf(strStatus);
+	}
+	public String toString() {
+		return name;
+	}
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public Date getReceiveDate() {
+		return receiveDate;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public int compareTo(Movie m) {
+		int compare = this.releaseDate.compareTo(m.getReleaseDate());
+		return compare;
+	}
+	public void setName(String newName) {
+		this.name = newName
+	}
+}
