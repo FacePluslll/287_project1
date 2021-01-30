5f01a1a7f786fcbe7865e9f51656980e8ecbd138
nm811 <nmetlapalli1@gmail.com>
2021-01-30 12:46:58 -0600

    This file contains all of the set methods for the Movie class. These methods allow us to change an attribute of the Movie class (name, releaseDate, description, etc.).

--- Movie.java
+++ Movie.java
@@ -46,6 +46,19 @@ public class Movie implements Comparable<Movie> {
 		return compare;
 	}
 	public void setName(String newName) {
-		this.name = newName
+		this.name = newName;
+	}
+	// mov = new Movie(name, releaseDate, description, receiveDate, strStatus);
+	public void setReleaseDate(Date newReleaseDate) {
+		this.releaseDate = newReleaseDate;
+	}
+	public void setDescription(String newDescription) {
+		this.description = newDescription;
+	}
+	public void setReceiveDate(Date newReceiveDate) {
+		this.receiveDate = newReceiveDate;
+	}
+	public void setReceiveDate(String newStrStatus) {
+		this.status = Status.valueOf(newStrStatus);
 	}
 }
